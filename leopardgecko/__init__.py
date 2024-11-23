abdf14ddfa408eb6c893056d23649b52daa81648
Luis Perdigao <perdigao1@yahoo.com>
2024-11-23 23:29:17 +0000

    bumped version to 1.2

--- leopardgecko/__init__.py
+++ leopardgecko/__init__.py
@@ -44,7 +44,7 @@ from . import ScoreData, Pcrit, metrics, AvgPool
 from . import hvectstatstools
 from . import hvect_plot_2class, hvect_plot_3class
 
-__version__ = "1.1"
+__version__ = "1.2"
 
 def lizzie():
     '''
